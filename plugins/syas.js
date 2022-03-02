4051e0dfbf174c8b1ff5cb98218f6c98a38e916c
lahkogituh <100285530+lahkogituh@users.noreply.github.com>
2022-03-02 23:07:57 +0700

    Update syas.js

--- plugins/syas.js
+++ plugins/syas.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(`https://i.ibb.co/rkTg7B0/donasi.jpg`)).buffer(), `
 ┌〔 𝘿𝙤𝙣𝙖𝙨𝙞 • 𝙀𝙢𝙤𝙣𝙚𝙮 〕
-├ _DANA_ : *0823-3460-2688*
-├ GOPAY : *0823-3460-2688*
-├ *SELAIN DANA VIA QR AJA ATAU GOPAY*
+├ _DANA_ : *0895-3496-13366*
+├ GOPAY : *0895-3496-13366*
+├ *SELAIN DANA VIA ATAU GOPAY*
 └────
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
